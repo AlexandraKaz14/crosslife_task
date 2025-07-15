be50f9f70fc3b60a12021e519769f5aceaf1ebaf
Aleksandra Kazarina <saschakazarina@gmail.com>
2025-07-16 01:15:52 +0300

    добавила поля и прописала отношения в моделях, создала фабрики и заполнила таблицы в бд

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'number',
+        'status',
+        'user_id',
+    ];
+
+    public const STATUS_DRAFT = 'draft';
+    public const STATUS_APPROVED = 'approved';
+
+    public const STATUSES = [
+        self::STATUS_DRAFT,
+        self::STATUS_APPROVED,
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory(100)->create();
+        Product::factory(500)->create();
+        Order::factory(100)->create();
+        OrderItem::factory(100)->create();
     }
 }
